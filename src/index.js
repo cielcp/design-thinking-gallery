bd503f7411aba4abbd347a1d5607f5a6e15fb4b4
cielcp <117504912+cielcp@users.noreply.github.com>
2024-01-31 08:33:17 +0000

    Deploying to gh-pages from @ cielcp/design-thinking-gallery@39e485a9f8a16d69eb7ab2b399bdc20361cac5bb 🚀

--- src/index.js
+++ src/index.js
@@ -97,5 +97,5 @@ regl.frame(({
 
 // Delayed alert to show after rendering
 setTimeout(() => {
-    alert("WELCOME TO THE A SCHOOL GALLERY\n\n  On mobile, tap the floor to navigate around the space\n\n  Browse through works created by your peers\n\n  Email your image and title to ccp7gcp to have your work displayed :)");
+    alert("WELCOME TO THE A SCHOOL GALLERY\n\nOn mobile, tap the floor to navigate around the space\n\nBrowse through works created by your peers\n\nEmail your image and title to ccp7gcp to have your work displayed :)");
 }, 200);
