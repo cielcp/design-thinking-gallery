dba861128b95117a7364ccc37f318c808623a968
cielcp <117504912+cielcp@users.noreply.github.com>
2024-06-02 05:47:46 +0000

    Deploying to gh-pages from @ cielcp/kevday2@90693d5352cfbb5320c8f677d8ea37cb8a89b8a8 🚀

--- src/index.js
+++ src/index.js
@@ -97,5 +97,5 @@ regl.frame(({
 
 // Delayed alert to show after rendering
 setTimeout(() => {
-    alert("to my lovely boyfriend,\n\ni am so grateful for you! ty for being my partner, duo, chauffeur, chef, etc. i hope you enjoy this collection of memories from 21.\n\nso much love,\nciel");
+    alert("to my lovely boyfriend,\n\ni am so grateful for you! ty for being my partner, duo, chauffeur, chef, everything. i hope you enjoy this collection of mems from 21.\n\nso much love,\nciel");
 }, 200);
